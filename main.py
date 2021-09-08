9d48b82a9d9290b3a04c1d668aa18e3c2d8cc66d
N Padma Keerthi <80039790+npkeerthi@users.noreply.github.com>
2021-09-08 19:15:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome To The Band Name Generator.")
+n=input("Whats the name of city you grew up in? \n")
+p=input("Whats the name of your pet? \n")
+print(" Your Band Name could be "+n+" "+p+"s")
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+
